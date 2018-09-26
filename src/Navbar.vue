f2584da915fc939169664843404c8e3a05deec77
Arie Shapiro <aryehshapiro@gmail.com>
2018-09-26 18:09:01 +0200

    first commit

---
+++ src/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <div class="nav-bar">
+      <div class="nav-logo">
+        <img src="@/assets/trendhaus-logo.webp" alt="">
+      </div>
+      <div class="nav-links">
+        <router-link class="nav" to="/">Unternehmen</router-link> 
+        <router-link class="nav" to="/about">Projekte</router-link>
+        <router-link class="nav" to="/about">Investments</router-link>
+        <router-link class="nav" to="/about">Partners</router-link>
+        <router-link class="nav" to="/about">Kontakt</router-link>
+      </div>
+    </div>
+    <router-view/>
+  </section>  
+</template>
+
+<style scoped>
+.nav {
+  text-decoration: none;
+  padding: 2%;
+}
+.nav-bar {
+  display: flex;
+  justify-content: space-between;
+  border-top: 5px solid yellow;
+  border-bottom: 5px solid yellow;
+}
+img {
+  width: 100%;
+  height: auto;
+}
+.nav-logo {
+  width: 25%;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,125 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<section>
+  <h1>This is a home page</h1>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+     Ut iure sit quia magnam. Voluptatem, iusto ea vitae, quos quibusdam optio
+      vero aperiam quam magnam similique, dicta tempore minima quasi earum.
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex similique corporis 
+      non alias nulla numquam cum, officiis excepturi. Commodi eum alias sed voluptas,
+       nihil necessitatibus atque ea officia debitis. Odio! Lorem ipsum dolor sit amet 
+       consectetur adipisicing elit. Aliquid rem possimus nisi maiores nihil, ut praesentium 
+       culpa exercitationem eligendi, expedita excepturi repudiandae tenetur. Praesentium velit
+        qui maxime repudiandae officia at!</p>
+</section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
     HelloWorld
   }
-}
+};
 </script>
